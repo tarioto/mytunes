abff258d9893e3825a49760787e0d5dd462da573
tarioto <timarioto@gmail.com>
2016-09-28 21:13:01 -0700

    Complete songModel.js Complete adding songs to queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,31 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
+  initialize: function(song) {
+    
+    if (song) {
+      this.add(song);  
+    }
+
+    this.on('add', function(song) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', collection.remove(this));
+      // this.remove(this.at(0)));
+      // remove());
+      // console.log(this));
+    // function(song) {
+    //   console.log(song);
+    //   this.remove(song);
+    //   // this.model.dequeue;
+    // });
+  },
+
+  playFirst: function() {
+
   }
 
 });
