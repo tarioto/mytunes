212c5bd329c9cbde0f9216e0a6930020097afcbf
tarioto <timarioto@gmail.com>
2016-09-29 11:13:14 -0700

    Complete SongQueueView.js & SongQueueEntryView.js specs Add Song Queue to index.html, partial functionality

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -2,9 +2,9 @@
 var AppView = Backbone.View.extend({
 
   initialize: function(params) {
-    this.playerView = new PlayerView({model: this.model.get('currentSong')});
-    this.libraryView = new LibraryView({collection: this.model.get('library')});
-
+    this.playerView = new PlayerView({model: this.model.get('currentSong')}); // Songs Model
+    this.libraryView = new LibraryView({collection: this.model.get('library')}); // Songs Model
+    this.songQueueView = new SongQueueView({collection: this.model.get('queue')});
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
@@ -15,7 +15,8 @@ var AppView = Backbone.View.extend({
   render: function() {
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,6 +1,8 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
     // this.collection.on('add', function() {
     this.render();
@@ -8,7 +10,16 @@ var SongQueueView = Backbone.View.extend({
   },
 
   render: function() {
-    return this.$el;
+    
+    this.$el.children().detach();
+
+    console.log(this);
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
+
 
 });
